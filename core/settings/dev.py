f7198e4cc0ebc804b91c11067dfaa9e1d5f54ca5
apl-mhd <apl.mhd@gmail.com>
2026-03-10 09:19:56 +0600

    build image

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -6,13 +6,20 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': 'db-container',# os.getenv('DB_HOST'),
         'PORT': 5432
     }
 }
